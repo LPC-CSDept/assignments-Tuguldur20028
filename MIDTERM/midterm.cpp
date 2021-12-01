1a12e8f04bbe535d71265719f913289b4f855a68
Tuguldur20028 <69940654+Tuguldur20028@users.noreply.github.com>
2021-12-01 02:50:49 +0000

    midterm

--- MIDTERM/midterm.cpp
+++ MIDTERM/midterm.cpp
@@ -55,3 +55,73 @@ ostream& operator<<(ostream& COUT, Student& E)
 void binarySearch(Student array[], int, int);
 void bubbleSortbyScores(Student [], int );
 void bubbleSortbyID(Student [], int );
+int main() {
+  cout << "Question 1: " << endl;
+
+  cout << endl;
+  
+  NumClass Aden(1,10);
+  cout << endl;
+
+  NumClass Anthony(2,20);
+  
+  printNumbers(Aden);
+  cout << endl;
+  cout << endl;
+  printNumbers(Anthony);
+
+  cout << endl;
+  cout << endl;
+
+  int ben = Aden > Anthony;
+
+  cout << "Result: " << ben << endl;
+
+  cout << "Total Number of Objects: " << Anthony.getNumberofObjects() << endl;
+
+  cout << " " << endl;
+  cout << endl;
+
+  cout << "Question 2: " << endl;
+
+  Student person[N];
+
+  fstream import;
+
+  if(import){
+    cout << "File access, Successful!" << endl;
+  } else cout << " Not found, Error." << endl;
+
+  int e = 0;
+
+  while(e < N){
+    import >> person[e];
+    cout << person[e];
+    e++;
+  }
+  bubbleSortbyScores(person, N);
+
+  cout << endl;
+  cout << endl;
+  cout << "Bubble Sort: " << endl;
+  for(int i = 0; i < N; i++){
+    cout << i + 1 << "."; 
+    cout << person[i] << endl;
+  }
+
+  bubbleSortbyID(person, N);
+
+  cout << endl;
+  cout << "Please enter the ID number of the students you would like to find: ";
+  int ID;
+  cin >> ID;
+  binarySearch(person, N, ID);
+} 
+
+void binarySearch(Student array[], int N, int target)
+{
+  bool found = false;
+  int first = 0;
+  int mid;
+  int last = N; 
+  int result;
