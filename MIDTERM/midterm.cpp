e0f5aa80f4739b350037134c7cb84b0e890d51a3
Tuguldur20028 <69940654+Tuguldur20028@users.noreply.github.com>
2021-12-01 02:27:29 +0000

    MIDTERM

--- MIDTERM/midterm.cpp
+++ MIDTERM/midterm.cpp
@@ -7,7 +7,51 @@
 
 using namespace std;
 
-int NumClass::NumberOfObjects = 0;
+int NumClass::NumberofObjects = 0;
 static int POS;
 
 const int N = 10;
+void operator>>(fstream& IMPORT, Student& array)
+{
+
+  IMPORT.open("MIDTERM/students.txt", ios :: in);
+
+  int sid;
+  string name;
+  double temp;
+  vector <double> scores;
+
+  IMPORT.seekg(POS);
+    IMPORT >> sid;
+    IMPORT >> name;
+    for(int i = 0; i < 3; i++)
+    {
+      IMPORT >> temp;
+      scores.push_back(temp);
+    }
+      array.setID(sid);
+      array.setName(name);
+      for(int q  = 0; q < 3; q++)
+      {
+        array.setScores(scores[q]);
+      }
+  
+  POS = IMPORT.tellg();
+  POS += 1;
+    
+  IMPORT.close();
+
+}
+
+ostream& operator<<(ostream& COUT, Student& E)
+{
+  COUT << endl;
+  COUT << E.getID() << " ";
+  COUT << E.getName() << " ";
+  COUT << E.getTotalSum() << " ";
+  return COUT;
+}
+
+void binarySearch(Student array[], int, int);
+void bubbleSortbyScores(Student [], int );
+void bubbleSortbyID(Student [], int );
