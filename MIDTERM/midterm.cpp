65515f53eede0e53f6c457ea2994da4770540779
Tuguldur20028 <69940654+Tuguldur20028@users.noreply.github.com>
2021-12-01 02:12:41 +0000

    have a little bit distraction by zoom meeting :))

---
+++ MIDTERM/midterm.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "numbers.hpp"
+#include "students.hpp"
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int NumClass::NumberOfObjects = 0;
+static int POS;
+
+const int N = 10;
