b9d3db3f667e7f13e2f750e1a3a9d9f6b88a512e
Tuguldur20028 <69940654+Tuguldur20028@users.noreply.github.com>
2021-12-01 01:54:56 +0000

    midterm

--- MIDTERM/students.hpp
+++ MIDTERM/students.hpp
@@ -1,7 +1,6 @@
 #ifndef 	STUDENT_H
 #define 	STUDENT_H
 
-
 #include <string>
  
 class Student{
@@ -14,4 +13,43 @@ class Student{
       ID = 0;
       name = "No Data";
     };
+    Student(int sID, string sName, vector <int> & E){
+      ID = sID;
+      name = sName;
+      for(int i = 0; sizeof(E); i++){
+        score.push_back(E[i]);
+      }
+    }
+    void setScores(double q){
+      score.push_back(q);
+    };
+
+    void setName(string sName){
+      name = sName;
+    }
+    void setID(int sID){
+      ID = sID;
+    }
     
+    void getScores(){
+      for(int i = 0; i < score.size(); i++){
+       cout << "Score " << i << " : " << score[i];
+      };
+    };
+
+    string getName(){
+      return name;
+    }
+
+    int getID() {
+      return ID;
+    }
+
+    int getTotalSum(){
+      int total = 0;
+      for(int i =0; i < 3; i++){
+        total += score[i];
+      }
+      return total;
+    }
+};
