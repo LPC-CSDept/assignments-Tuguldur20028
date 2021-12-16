3508d2d4e3bcffa7baa26c9f1ba1ee387ffd4d37
Tuguldur20028 <69940654+Tuguldur20028@users.noreply.github.com>
2021-12-16 21:26:11 +0000

    finall

--- FINAL/question1.cpp
+++ FINAL/question1.cpp
@@ -11,3 +11,37 @@ using namespace std;
 static int POS;
 static string fileName;
 const int COURSES = 10;
+
+void operator >>(fstream& FILE_IN, Course& array){
+  int id;
+  string name;
+  int credit;
+   
+  FILE_IN.open(fileName, ios :: in);
+
+  FILE_IN.seekg(POS);
+
+  FILE_IN >> id;
+  FILE_IN >> name;
+  FILE_IN >> credit;
+
+  array.setID(id);
+  array.setName(name);
+  array.setCredit(credit);
+
+  POS = FILE_IN.tellg();
+  POS += 1;
+
+  FILE_IN.close();
+}
+ostream& operator<<(ostream& COUT, Course& LESSONS){
+  COUT << LESSONS.getID() << " ";
+  COUT << LESSONS.getName() << " ";
+  COUT << LESSONS.getCredit() << " ";
+  return COUT;
+}
+
+int main() {
+
+  fstream FILE_IN;
+  Course lessons[10];
