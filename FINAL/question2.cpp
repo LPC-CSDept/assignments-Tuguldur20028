07cddcdcb0b42897f800b8fc0d4f720759d4addb
Tuguldur20028 <69940654+Tuguldur20028@users.noreply.github.com>
2021-12-16 22:02:48 +0000

    final

---
+++ FINAL/question2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include "course.cpp"
+
+int partition (Course array[], int , int );
+void quickSort(Course array[], int, int );
+void swap(int* a, int* b);
+void displayArray(Course arr[], int size);
+
+static int POS;
+static string fileName;
+const int COURSES = 10;
+
+void operator>>(fstream& FILE_IN, Course& array){
+  int id;
+  string name;
+  int credit;
+  
+  FILE_IN.open(fileName, ios :: in);
+
+  FILE_IN.seekg(POS);
+
+  FILE_IN >> id;
+  FILE_IN >> name;
+  FILE_IN >> credit;
+
+  array.setID(id);
+  array.setName(name);
+  array.setCredit(credit);
+
+  POS = FILE_IN.tellg();
+  POS += 1;
+
+  FILE_IN.close();
+  
+}
