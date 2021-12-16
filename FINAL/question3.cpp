4af462dab8a82b9ddf9ac3802673f377d4e84311
Tuguldur20028 <69940654+Tuguldur20028@users.noreply.github.com>
2021-12-16 23:54:20 +0000

    final

--- FINAL/question3.cpp
+++ FINAL/question3.cpp
@@ -30,3 +30,38 @@ double operator > (const ProductWorker &Q, const ProductWorker &F){
   cout << "If the result is: 0, " <<Q.employeeName << " is not paid more." << endl;
   return Q.hourlyPayRate > F.hourlyPayRate;
 }
+void HighestPaid(ProductWorker *array){
+  ProductWorker Result;
+  for(int i = 0; i < 10; i++)
+    if(array[i].getPay() > maxPay){
+      maxPay = array[i].getPay();
+      Result = array[i];
+    }
+    
+ cout << Result;
+}
+void operator>>(fstream& FILE_IN, ProductWorker& array){
+  int number, SHIFT;
+  string name, date;
+  double pay;
+  
+  FILE_IN.open(fileName, ios :: in);
+  FILE_IN.seekg(POS);
+  FILE_IN >> number;
+  FILE_IN >> name;
+  FILE_IN >> date;
+  FILE_IN >> SHIFT;
+  FILE_IN >> pay;
+
+  array.setName(name);
+  array.setID(number);
+  array.setHired(date);
+  array.setShift(SHIFT);
+  array.setPay(pay);
+
+  POS = FILE_IN.tellg();
+  POS += 1;
+
+  FILE_IN.close();
+  
+}
